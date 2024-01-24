7ee9399fec8ee994e69b6b22e5dec58a8420ba48
Muhammad Arslan <you@example.com>
2024-01-24 15:24:36 +0500

    first commit

--- src/screens/Carousel.js
+++ src/screens/Carousel.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Pic1 from "../assets/cvr-5.jpg";
+import cvr from "../assets/cvr-1.jpg";
 import { Link } from "react-router-dom";
 
 const Carousel = () => {
   return (
     <>
-      <img className="header-img mt-10 pt-6" src={Pic1} />
+      <img className="header-img mt-10 pt-6" src={cvr} />
       <div className="header-content flex flex-col float-right relative">
-        <div className=" ">
-          <h2 className=" text-4xl font-extrabold leading-10 tracking-tight text-gray-200  sm:text-5xl sm:leading-none md:text-6xl">
+        <div className=" flex  flex-col -mt-20 ml-20 ">
+          <h2 className="text-right text-4xl font-extrabold leading-10 tracking-tight text-gray-200  sm:text-5xl sm:leading-none md:text-6xl">
             <span className="bg-gray-800">DREAM</span>
 
             <span
@@ -18,16 +18,16 @@ const Carousel = () => {
               STORE
             </span>
           </h2>
-          <p className="text-base text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+          <p className="rexr-right text-base text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             is offering flat <span className="font-bold text-white">50%</span>{" "}
             <span className="font-bold white" style={{ color: "" }}>
               discount
             </span>{" "}
             on entire stock. Avail the offer now!
           </p>
-          <div className="flex ">
+          <div className="flex justify-end ">
             <Link to="/products" className=" avail-btn rounded-md">
-              <button className="px-1.5  text-white font-bold transition duration-150 ease-in-out bg-red-500 hover:bg-white rounded-md hover:text-red-500 hover:font-bold ">
+              <button className="px-1.5   text-white font-bold transition duration-150 ease-in-out bg-red-500 hover:bg-white rounded-md hover:text-red-500 hover:font-bold ">
                 Avail Now!
               </button>
             </Link>
